d9a1580bf957c384437ddce541d803686049877b
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2024-05-29 17:30:55 +0700

    change host

--- main.py
+++ main.py
@@ -62,6 +62,6 @@ def get_latest_data():
 # app.run(host='localhost', port=5000)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
