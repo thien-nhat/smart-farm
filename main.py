c65750c430205a55f012a9968cfe2ca365c6bc1a
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2024-06-06 01:12:54 +0700

    change port

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def get_latest_data():
 # app.run(host='localhost', port=5000)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
-    # app.run(host="0.0.0.0", port=10000, debug=False)
+    # app.run(host='localhost', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=False)
 
 
